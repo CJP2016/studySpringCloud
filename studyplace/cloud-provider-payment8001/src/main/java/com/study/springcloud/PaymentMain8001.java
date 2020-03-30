444ed297bdf1d8601934a911634c5f6aad071f73
kipp <kipp.cai@tyj365.com>
2020-03-30 21:52:59 +0800

    创建

---
+++ studyplace/cloud-provider-payment8001/src/main/java/com/study/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    /*
+     * @Author kipp
+     * @Description 
+     * @Date 21:24 2020/3/30
+     * @Param [args]
+     * @return void
+     **/
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args); 
+    }
+}
