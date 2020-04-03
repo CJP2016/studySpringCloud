5ef670753e4ac5426285ce671fe0a2323b2e0236
kipp <kipp.cai@tyj365.com>
2020-04-03 22:16:15 +0800

    64章--hystrixDashBoard hystrix监控

---
+++ studyplace/cloud-consumer-hystrix-dashboard9001/src/main/java/com/study/springcloud/HystrixDashBoardMain9001.java
@@ -0,0 +1,20 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @ClassName HystrixDashBoardMain9001
+ * @Description
+ * @Author Administrator
+ * @Date 2020/4/3 21:54
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashBoardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashBoardMain9001.class,args);
+    }
+}
