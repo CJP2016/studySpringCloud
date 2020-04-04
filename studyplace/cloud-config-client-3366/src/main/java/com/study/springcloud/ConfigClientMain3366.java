bbaaf5208aa13663685b9290411b3840dd96d50a
kipp <kipp.cai@tyj365.com>
2020-04-04 13:33:08 +0800

    83章--Spring Cloud Bus

---
+++ studyplace/cloud-config-client-3366/src/main/java/com/study/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigClientMain3366
+ * @Description
+ * @Author Administrator
+ * @Date 2020/4/4 12:59
+ * @Version 1.0
+ **/
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
