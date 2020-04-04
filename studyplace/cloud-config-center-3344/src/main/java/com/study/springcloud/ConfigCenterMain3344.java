24805c93b45075f838b0a1d06028ee6ebc645117
kipp <kipp.cai@tyj365.com>
2020-04-04 12:22:37 +0800

    77章——config

---
+++ studyplace/cloud-config-center-3344/src/main/java/com/study/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,21 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigCenterMain3344
+ * @Description
+ * @Author Administrator
+ * @Date 2020/4/4 11:25
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ studyplace/cloud-config-client-3355/src/main/java/com/study/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,21 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigClientMain3355
+ * @Description
+ * @Author Administrator
+ * @Date 2020/4/4 12:03
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+
+}
